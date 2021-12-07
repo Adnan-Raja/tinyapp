9a12e103595a1b16b88b8d9a9e2044db24a1b8e0
Adnan-Raja <adnan.saeed.raja@gmail.com>
2021-12-07 00:18:36 -0500

    included header

--- express_server.js
+++ express_server.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = 8080;
 
 app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -12,12 +13,24 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls", (req, res) => {
+  const templaterVars = { urls: urlDatabase };
+  //console.log(urls)
+  res.render("urls_index", templaterVars);
+});
+
+  app.get("/urls/:shortURL", (req, res) => {
+ // console.log(req.params)
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
